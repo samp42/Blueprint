cbcf31e341ccd5a235e4dd4389531a21466f8549
Samuel Proulx <samuelproulx26@gmail.com>
2020-01-18 23:03:35 -0500

    Initial Commit

---
+++ Blueprint/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-18.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Blueprint/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-18.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
