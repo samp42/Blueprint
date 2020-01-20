6a8baa96973147f09e0360d64f5ed3852e756b06
Samuel Proulx <samuelproulx26@gmail.com>
2020-01-19 22:40:22 -0500

    initialisation of gitignore

---
+++ Blueprint/InfoView.swift
@@ -0,0 +1,21 @@
+//
+//  InfoView.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-19.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView()
+    }
+}
