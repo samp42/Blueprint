63a63a2555be3e8dd6d4fb84a1eb1d7b563272b2
Samuel Proulx <samuelproulx26@gmail.com>
2020-01-31 14:01:22 -0500

    create TeamDetailView.swift

---
+++ Blueprint/Teams/TeamDetailView.swift
@@ -0,0 +1,34 @@
+//
+//  TeamDetailView.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-31.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import SwiftUI
+
+struct TeamDetailView: View {
+    var body: some View {
+        VStack{
+            Text("<Teams")
+            HStack{
+                Image("3990_2018")
+                .resizable()
+                    .frame(maxHeight: 200)
+                    .frame(maxWidth: 200)
+                VStack{
+                    Text("Tech for Kids")
+                    Text("3990")
+                    Spacer("Montréal, Québec, Canada")
+                }.frame(height: 200)
+            }
+        }
+    }
+}
+
+struct TeamDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamDetailView()
+    }
+}
---
+++ Blueprint/Teams/TeamView.swift
@@ -0,0 +1,21 @@
+//
+//  TeamView.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-31.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import SwiftUI
+
+struct TeamView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TeamView_Previews: PreviewProvider {
+    static var previews: some View {
+        TeamView()
+    }
+}
