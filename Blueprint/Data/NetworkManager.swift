f268254723fcd392676b7098b10d2b815e2e3753
Samuel Proulx <samuelproulx26@gmail.com>
2020-01-31 14:06:46 -0500

    create network manager

---
+++ Blueprint/Data/NetworkManager.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-31.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import Foundation
+
+class Networkmanager{
+    
+}
