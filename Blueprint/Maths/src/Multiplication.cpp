c38131aa9774f4e5e69db90bf4796a2c0da51fb5
Samuel Proulx <samuelproulx26@gmail.com>
2020-02-09 00:05:40 -0500

    Advance matrix multiplication

---
+++ Blueprint/Maths/src/Multiplication.cpp
@@ -0,0 +1,24 @@
+//
+//  Multiplication.cpp
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-02-08.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+/**
+ * Functions to multiply matrices
+ * 
+*/
+
+#include <iostream>
+#include <vector>
+
+template<class T>{
+    std::vector<std::vector<T>> MultiplyM(std::vector<std::vector<T>> MatrixA, std::vector<std::vector<T>> MatrixB){
+
+        array ResultMatrix = new <T>[MatrixA.size()][MatrixB[0].size()]; //array = rows of A x columns of B
+        for(int i=0; i<)
+
+    }
+}
