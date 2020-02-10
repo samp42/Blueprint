482c30e85d54e12c7a905929159f668c11b57634
Samuel Proulx <samuelproulx26@gmail.com>
2020-02-10 13:12:51 -0500

    update project

---
+++ Blueprint/ScoreBoard/ScoreBoardView 3.swift
@@ -0,0 +1,21 @@
+//
+//  ScoreBoardView.swift
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-01-22.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+import SwiftUI
+
+struct ScoreBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScoreBoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScoreBoardView()
+    }
+}
