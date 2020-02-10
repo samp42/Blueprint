0d99820265e6eeba865c14e25d815b276ae1b001
Samuel Proulx <samuelproulx26@gmail.com>
2020-02-10 11:14:30 -0500

    base functions for matrices

--- Blueprint/Maths/src/Multiplication.cpp
+++ Blueprint/Maths/src/Multiplication.cpp
@@ -7,18 +7,21 @@
 //
 
 /**
- * Functions to multiply matrices
- * 
+ * Function to multiply matrices
+ * Multiplies the two elements and puts it into an array representation of a sparse matrix
 */
 
 #include <iostream>
 #include <vector>
 
-template<class T>{
-    std::vector<std::vector<T>> MultiplyM(std::vector<std::vector<T>> MatrixA, std::vector<std::vector<T>> MatrixB){
-
-        array ResultMatrix = new <T>[MatrixA.size()][MatrixB[0].size()]; //array = rows of A x columns of B
-        for(int i=0; i<)
-
+template<class T>;
+std::vector<std::vector<T>> MultiplyM(std::vector<std::vector<T>> MatrixA, std::vector<std::vector<T>> MatrixB){
+    array ResultMatrix = new <T>[MatrixA.size()][MatrixB[0].size()]; //array = rows of A x columns of B
+    
+    for(int i in MatrixA[0].size()){
+        for(int j in MatrixA.size()){
+            if()
+        }
     }
+    return ResultMatrix;
 }
---
+++ Blueprint/Maths/src/Transpose.cpp
@@ -0,0 +1,33 @@
+//
+//  Transpose.cpp
+//  Blueprint
+//
+//  Created by Samuel Proulx on 2020-02-09.
+//  Copyright © 2020 SP. All rights reserved.
+//
+
+/**
+ * Function to transpose a matrix
+ * Transpose an array representation of a sparse matrix
+*/
+
+/**
+ * 
+ * vector<[row<short>, column<short>, value<float>]>
+*/
+
+#include <iostream>
+#include <vector>
+
+template<class T>{
+    std::vector<[T]> MultiplyM(std::vector<[T]> MatrixA, std::vector<[T]> MatrixB){
+
+        array ResultMatrix = new <T>[MatrixA.size()][MatrixB[0].size()]; //array = rows of A x columns of B
+        for(int i in MatrixA[0].size() - 1){
+            for(int j in MatrixA.size() - 1){
+                if()
+            }
+        }
+        return ResultMatrix;
+    }
+}
